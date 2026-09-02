e912dae22ff9f583b9050003ea737a6d5d77cbbb
Thales Alves <thalesalvs21@gmail.com>
2026-09-01 23:34:10 -0300

    Criação de getters e setters

--- src/main/java/br/com/hw/hwatendimento/model/Cliente.java
+++ src/main/java/br/com/hw/hwatendimento/model/Cliente.java
@@ -10,4 +10,41 @@ public class Cliente {
     private String nomeEmpresa;
     private String telefone;
     private LocalDateTime criadoEm;
+
+    public Cliente(){
+    }
+    public String getTipo(){
+        return tipo;
+    }
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNomeEmpresa(){
+        return nomeEmpresa;
+    }
+    public void setNomeEmpresa(String nomeEmpresa){
+        this.nomeEmpresa = nomeEmpresa;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+    public LocalDateTime getCriadoEm(){
+        return criadoEm;
+    }
+    public void setCriadoEm(LocalDateTime criadoEm){
+        this.criadoEm = criadoEm;
+    }
 }
