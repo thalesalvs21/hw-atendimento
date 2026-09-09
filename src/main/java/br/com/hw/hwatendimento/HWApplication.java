c8179d20bf419df43e16699ce1f312112df3af5d
Thales Alves <thalesalvs21@gmail.com>
2026-09-08 22:04:32 -0300

    Update 1.0.8 - Ajustando detalhes da tela de cadastro

--- src/main/java/br/com/hw/hwatendimento/HWApplication.java
+++ src/main/java/br/com/hw/hwatendimento/HWApplication.java
@@ -3,6 +3,7 @@ package br.com.hw.hwatendimento;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,12 @@ public class HWApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HWApplication.class.getResource("atendimento-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().addAll(
+                new Image(getClass().getResourceAsStream("/br/com/hw/hwatendimento/assets/icone-16.png")),
+                new Image(getClass().getResourceAsStream("/br/com/hw/hwatendimento/assets/icone-32.png")),
+                new Image(getClass().getResourceAsStream("/br/com/hw/hwatendimento/assets/icone-48.png")),
+                new Image(getClass().getResourceAsStream("/br/com/hw/hwatendimento/assets/icone-256.png"))
+        );
         stage.setScene(scene);
         stage.show();
     }
